18b547049014afa797e710821ce97f387c809627
Christopher Peplin <peplin@bueda.com>
2011-04-09 15:07:25 -0400

    If no UUID provided to NodeHandler, load ourselves.
    Also, only shutdown the node if we deleted ourselves (not any node).

--- astral/api/handlers/node.py
+++ astral/api/handlers/node.py
@@ -11,14 +11,19 @@ class NodeHandler(BaseHandler):
         """Remove the requesting node from the list of known nodes,
         unregistering the from the network.
         """
-        if node_uuid == None:
-          node_uuid = Node.me()
-
-        node = Node.get_by(uuid=node_uuid)
+        if node_uuid:
+            node = Node.get_by(uuid=node_uuid)
+        else:
+            node = Node.me()
         node.delete()
+
         closest_supernode = Node.closest_supernode()
         if closest_supernode:
             log.info("Notifying closest supernode %s that %s was deleted",
                     closest_supernode, node)
             NodesAPI(closest_supernode.absolute_url()).delete(node)
-        raise KeyboardInterrupt
+
+        if node == Node.me():
+            # TODO kind of a shortcut to shutting down, but should be a bit more
+            # formal
+            raise KeyboardInterrupt
