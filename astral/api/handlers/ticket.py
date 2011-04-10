27b0542cd6bccb65aa07c41a76b4365d25b688ed
Christopher Peplin <peplin@bueda.com>
2011-04-10 19:08:07 -0400

    Disambiguate between self and remote tickets when retreiving.

--- astral/api/handlers/ticket.py
+++ astral/api/handlers/ticket.py
@@ -7,13 +7,16 @@ logger = logging.getLogger(__name__)
 
 class TicketHandler(BaseHandler):
     def _load_ticket(self, stream_id, destination_uuid):
-        if destination_uuid:
-            node = Node.get_by(uuid=destination_uuid)
-        else:
-            node = Node.me()
+        if not destination_uuid:
+            return Ticket.get_by(stream_id=stream_id, source=Node.me(),
+                    destination=Node.me())
 
-        if node:
-            return Ticket.get_by(stream_id=stream_id, destination=node)
+        node = Node.get_by(uuid=destination_uuid)
+        query = Ticket.query.filter_by(stream_id=stream_id).filter_by(
+                destination=node)
+        if node == Node.me():
+                    query.filter(Ticket.source != Node.me())
+        return query.first()
 
     def delete(self, stream_id, destination_uuid=None):
         """Stop forwarding the stream to the requesting node."""
