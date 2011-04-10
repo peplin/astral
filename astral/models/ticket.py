90e79de74601fe7712e746c45deb2cd04435d2a6
Christopher Peplin <peplin@bueda.com>
2011-04-10 19:56:44 -0400

    Expire unconfirmed tickets after 1 day. Fixed #71.

--- astral/models/ticket.py
+++ astral/models/ticket.py
@@ -1,7 +1,9 @@
-from elixir import ManyToOne, Entity, Boolean, Field
-from elixir.events import after_insert
+from elixir import ManyToOne, Entity, Boolean, Field, DateTime
+from elixir.events import after_insert, before_insert
 import json
+import datetime
 
+from astral.conf import settings
 from astral.models.base import BaseEntityMixin
 from astral.models.event import Event
 from astral.models.node import Node
@@ -12,6 +14,7 @@ class Ticket(Entity, BaseEntityMixin):
     destination = ManyToOne('Node')
     stream = ManyToOne('Stream')
     confirmed = Field(Boolean, default=False)
+    created = Field(DateTime)
 
     def __init__(self, source=None, destination=None, *args, **kwargs):
         source = source or Node.me()
@@ -28,9 +31,27 @@ class Ticket(Entity, BaseEntityMixin):
                 'destination': self.destination.uuid,
                 'stream': self.stream_id}
 
+    @classmethod
+    def unconfirmed(cls, query=None):
+        query = query or cls.query
+        return query.filter_by(confirmed=False)
+
+    @classmethod
+    def old(cls, query=None):
+        query = query or cls.query
+        now = datetime.datetime.now()
+        one_day_ago = now - datetime.timedelta(
+                days=settings.UNCONFIRMED_TICKET_EXPIRATION)
+        return query.filter(Ticket.created < one_day_ago)
+
     @after_insert
     def emit_new_ticket_event(self):
         Event(message=json.dumps({'type': "ticket", 'data': self.to_dict()}))
+
+    @before_insert
+    def set_created_time(self):
+        if not self.created:
+            self.created = datetime.datetime.now()
 
     def __repr__(self):
         return u'<Ticket %s: %s from %s to %s>' % (
--- astral/node/daemon.py
+++ astral/node/daemon.py
@@ -1,7 +1,6 @@
 import threading
-import json
 
-from astral.models import Event
+from astral.models import Ticket
 
 import logging
 log = logging.getLogger(__name__)
@@ -14,9 +13,16 @@ class DaemonThread(threading.Thread):
         super(DaemonThread, self).__init__()
         self.daemon = True
 
+    def _cleanup_unconfirmed_tickets(self):
+        query = Ticket.unconfirmed()
+        query = Ticket.old(query=query)
+        for ticket in query:
+            log.info("Expiring unconfirmed ticket %s", ticket)
+            ticket.delete()
+
     def run(self):
         import time
         while self.is_alive():
             log.debug("Daemon thread woke up")
-            Event(message=json.dumps({'type': "update"}))
+            self._cleanup_unconfirmed_tickets()
             time.sleep(10)
