7253795f22ea3f2ed978f2f11e26ab00f7a4d4c1
Christopher Peplin <peplin@bueda.com>
2011-04-23 16:30:20 -0400

    Accept edits to the 'streaming' status in a GET request. :(

--- astral/api/handlers/stream.py
+++ astral/api/handlers/stream.py
@@ -1,3 +1,5 @@
+from tornado import web
+
 from astral.api.handlers.base import BaseHandler
 from astral.models import Stream
 
@@ -8,7 +10,15 @@ log = logging.getLogger(__name__)
 class StreamHandler(BaseHandler):
     def get(self, stream_slug):
         """Return metadata for the stream."""
-        self.write({'stream': Stream.get_by(slug=stream_slug).to_dict()})
+        # TODO accept edits to the 'streaming' attribute through GET, which
+        # while not ideal, is the easiest way to get around the same-origin
+        # policy in the browser.
+        stream = Stream.get_by(slug=stream_slug)
+        if not stream:
+            raise web.HTTPError(404)
+        if 'streaming' in self.request.arguments:
+            stream.streaming = self.request.arguments['streaming'][0] == 'true'
+        self.write({'stream': stream.to_dict()})
 
     def put(self, stream_slug):
         stream = Stream.get_by(slug=stream_slug)
