68c048f625aa0158f0117a737d94000da193df40
Christopher Peplin <peplin@bueda.com>
2011-04-24 15:01:23 -0400

    Convert JSON to unicode before saving to clear up a SQLAlchemy warning.

--- astral/models/event.py
+++ astral/models/event.py
@@ -14,6 +14,7 @@ class Event(BaseEntityMixin, Entity):
     message = Field(Unicode(96))
 
     def __init__(self, *args, **kwargs):
+        kwargs['message'] = unicode(kwargs['message'])
         super(Event, self).__init__(*args, **kwargs)
         EVENT_QUEUE.put(self)
 
