ef7dc2ffe86cc4bbc28aa648edb851e03e65c523
Christopher Peplin <peplin@bueda.com>
2011-04-27 23:56:27 -0400

    Don't propagate ticket deletes if they are unconfirmed.

--- astral/api/handlers/ticket.py
+++ astral/api/handlers/ticket.py
@@ -25,29 +25,32 @@ class TicketHandler(BaseHandler):
         if not ticket:
             raise HTTPError(404)
 
-        if ticket.destination == Node.me():
-            if self.request.remote_ip == '127.0.0.1':
-                log.info("User is canceling %s -- must inform sender",
-                        ticket)
+        if ticket.confirmed:
+            if ticket.destination == Node.me():
+                if self.request.remote_ip == '127.0.0.1':
+                    log.info("User is canceling %s -- must inform sender",
+                            ticket)
+                    TicketsAPI(ticket.source.uri()).cancel(
+                            ticket.absolute_url())
+                else:
+                    log.info("%s is being deleted, we need to find another for "
+                            "ourselves", ticket)
+                    try:
+                        TicketsHandler.handle_ticket_request(ticket.stream,
+                                ticket.destination)
+                    except HTTPError, e:
+                        log.warning("We lost %s and couldn't find a "
+                                "replacement to failover -- our stream is "
+                                "dead: %s", ticket, e)
+            elif self.request.remote_ip == ticket.source.ip_address:
+                log.info("%s is being deleted by the source, must inform the "
+                        "target %s", ticket, ticket.destination)
+                TicketsAPI(ticket.destination.uri()).cancel(
+                        ticket.absolute_url())
+            elif self.request.remote_ip == ticket.destination.ip_address:
+                log.info("%s is being deleted by the destination, must inform "
+                        "the source %s", ticket, ticket.source)
                 TicketsAPI(ticket.source.uri()).cancel(ticket.absolute_url())
-            else:
-                log.info("%s is being deleted, we need to find another for "
-                        "ourselves", ticket)
-                try:
-                    TicketsHandler.handle_ticket_request(ticket.stream,
-                            ticket.destination)
-                except HTTPError, e:
-                    log.warning("We lost %s and couldn't find a replacement "
-                            "to failover -- our stream is dead: %s", ticket,
-                            e)
-        elif self.request.remote_ip == ticket.source.ip_address:
-            log.info("%s is being deleted by the source, must inform the "
-                    "target %s", ticket, ticket.destination)
-            TicketsAPI(ticket.destination.uri()).cancel(ticket.absolute_url())
-        elif self.request.remote_ip == ticket.destination.ip_address:
-            log.info("%s is being deleted by the destination, must inform the "
-                    "source %s", ticket, ticket.source)
-            TicketsAPI(ticket.source.uri()).cancel(ticket.absolute_url())
         if ticket:
             ticket.delete()
             session.commit()
@@ -66,8 +69,8 @@ class TicketHandler(BaseHandler):
                 ticket.queue_tunnel_creation()
                 session.commit()
                 # TODO this is unideal, but we need to get the new port if it
-                # changed. combination of sleep and db flush seems to do it somewhat
-                # reliably, but it's still a race condition.
+                # changed. combination of sleep and db flush seems to do it
+                # somewhat reliably, but it's still a race condition.
                 import time
                 time.sleep(1)
             ticket = self._load_ticket(stream_slug, destination_uuid)
