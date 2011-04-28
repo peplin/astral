c354042be95d279f158a63b89c1f33dc48730cb8
Christopher Peplin <peplin@bueda.com>
2011-04-28 11:08:00 -0400

    Move ticket deletion handling code outside of the API request/response cycle.

--- astral/api/handlers/ticket.py
+++ astral/api/handlers/ticket.py
@@ -1,5 +1,6 @@
 from tornado.web import HTTPError
 import datetime
+import threading
 
 from astral.api.client import TicketsAPI
 from astral.api.handlers.base import BaseHandler
@@ -24,38 +25,7 @@ class TicketHandler(BaseHandler):
         ticket = self._load_ticket(stream_slug, destination_uuid)
         if not ticket:
             raise HTTPError(404)
-
-        if ticket.confirmed and not ticket.source == Node.me():
-            if ticket.destination == Node.me():
-                if self.request.remote_ip == '127.0.0.1':
-                    log.info("User is canceling %s -- must inform sender",
-                            ticket)
-                    TicketsAPI(ticket.source.uri()).cancel(
-                            ticket.absolute_url())
-                else:
-                    log.info("%s is being deleted, we need to find another for "
-                            "ourselves", ticket)
-                    # TODO disabled for now, since the web server will block
-                    # with these deadlocked requests
-                    #try:
-                    #    TicketsHandler.handle_ticket_request(ticket.stream,
-                    #            ticket.destination)
-                    #except HTTPError, e:
-                    #    log.warning("We lost %s and couldn't find a "
-                    #            "replacement to failover -- our stream is "
-                    #            "dead: %s", ticket, e)
-            elif self.request.remote_ip == ticket.source.ip_address:
-                log.info("%s is being deleted by the source, must inform the "
-                        "target %s", ticket, ticket.destination)
-                TicketsAPI(ticket.destination.uri()).cancel(
-                        ticket.absolute_url())
-            elif self.request.remote_ip == ticket.destination.ip_address:
-                log.info("%s is being deleted by the destination, must inform "
-                        "the source %s", ticket, ticket.source)
-                TicketsAPI(ticket.source.uri()).cancel(ticket.absolute_url())
-        if ticket:
-            ticket.delete()
-            session.commit()
+        TicketDeletionPropagationThread(ticket).start()
 
     def get(self, stream_slug, destination_uuid=None):
         ticket = self._load_ticket(stream_slug, destination_uuid)
@@ -87,4 +57,46 @@ class TicketHandler(BaseHandler):
             ticket.confirmed = self.get_json_argument('confirmed')
             if ticket.confirmed:
                 log.info("Confirmed %s", ticket)
+            session.commit()
+
+
+class TicketDeletionPropagationThread(threading.Thread):
+    """When a ticket is deleted, we may need to inform other nodes or find a
+    replacement for ourselves. We don't want to do this in-band with the delete
+    request because it can cause dead locking of API requests between nodes.
+    """
+    def __init__(self, ticket):
+        super(TicketDeletionPropagationThread, self).__init__()
+        self.ticket = ticket
+
+    def run(self):
+        if self.ticket.confirmed and not self.ticket.source == Node.me():
+            if self.ticket.destination == Node.me():
+                if self.request.remote_ip == '127.0.0.1':
+                    log.info("User is canceling %s -- must inform sender",
+                            self.ticket)
+                    TicketsAPI(self.ticket.source.uri()).cancel(
+                            self.ticket.absolute_url())
+                else:
+                    log.info("%s is being deleted, we need to find another for "
+                            "ourselves", self.ticket)
+                    try:
+                        TicketsHandler.handle_ticket_request(self.ticket.stream,
+                                self.ticket.destination)
+                    except HTTPError, e:
+                        log.warning("We lost %s and couldn't find a "
+                                "replacement to failover -- our stream is "
+                                "dead: %s", self.ticket, e)
+            elif self.request.remote_ip == self.ticket.source.ip_address:
+                log.info("%s is being deleted by the source, must inform the "
+                        "target %s", self.ticket, self.ticket.destination)
+                TicketsAPI(self.ticket.destination.uri()).cancel(
+                        self.ticket.absolute_url())
+            elif self.request.remote_ip == self.ticket.destination.ip_address:
+                log.info("%s is being deleted by the destination, must inform "
+                        "the source %s", self.ticket, self.ticket.source)
+                TicketsAPI(self.ticket.source.uri()).cancel(
+                        self.ticket.absolute_url())
+        if self.ticket:
+            self.ticket.delete()
             session.commit()
